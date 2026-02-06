a5d0390e97e98096c7cc5874fe53ee5abbd7b050
gcaanmm-maker <gcaanmm@gmail.com>
2026-02-06 20:04:41 -0300

    Delete from flask import Flask app = Flask(__name__)  @app.route('/') def hello():     return "<h1>Olá! Este app está rodando dentro de um Container! 🚀< directory

--- from flask import Flask app = Flask(__name__)  @app.route('/') def hello():     return "<h1>Olá! Este app está rodando dentro de um Container! 🚀</app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "<h1>Olá! Este app está rodando dentro de um Container! 🚀</h1>"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
